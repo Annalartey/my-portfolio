ed71f461d429c0d662f523a07c1f6a6ff73da697
Annalartey <74235952+Annalartey@users.noreply.github.com>
2021-06-22 20:32:07 +0000

    logo

--- src/component/WebHeader.js
+++ src/component/WebHeader.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import logo from '../images/logo-b.png'
 
 function WebHeader() {
     return (
-        <div className='design-header py-4 px-2 lg:px-40'>
-          <Link to="/"><h1>L.A.T</h1></Link>
+        <div className='flex flex-col lg:flex-row design-header px-2 lg:px-40'>
+          <Link to="/"><img className="w-20 h-20" src={logo} alt=""></img> </Link>
     
-          <div className='flex flex-row'>
+          <div className='flex flex-row pb-4'>
             <Link to="/design-website"><h3 className="font-bold">Websites</h3> </Link>
             <Link to="/design-graphics"> <h3>Graphics</h3> </Link>
             <Link to="/design-lyrics"> <h3>Lyrics</h3> </Link>
