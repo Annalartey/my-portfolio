55c034388135b1d54dd4c89cc86763d637afba1f
Annalartey <74235952+Annalartey@users.noreply.github.com>
2021-06-22 19:51:18 +0000

    commit

--- src/component/Designs.js
+++ src/component/Designs.js
@@ -16,14 +16,14 @@ function Designs() {
                 <div className="w-40 lg:w-20  h-1 bg-red-600 ml-14 mb-10 rounded-2xl"></div>
             </div>
             
-            <div className="lg:px-40 lg:w-full " >
-                <div className='lg:flex lg:flex-row lg:w-100% lg:pl-40' data-aos="fade-up">
-                <Link to="/design-website"> <div className='design-web lg:w-96 text-2xl font-semibold border-2 border-red-100 lg:mr-8 mt-8 mx-4 lg:mx-0 '>Coding Projects</div></Link>
-                <Link to="/design-graphics"><div className='design-graph lg:w-96 text-2xl  font-semibold border-2 border-red-100 mt-8 mx-4 lg:mx-0 '>Graphic Designs</div></Link>
+            <div className="px-10 lg:px-40 lg:w-full " >
+                <div className='flex-col lg:flex-row lg:w-100% lg:pl-40' data-aos="fade-up">
+                <Link to="/design-website"> <div className='design-web border-2 lg:w-96 lg:mr-8 mt-8 lg:mx-0 pt-10 text-2xl text-center font-semibold  '>Coding Projects</div></Link>
+                <Link to="/design-graphics"><div className='design-graph  border-2 lg:w-96 lg:mr-8 mt-8 lg:mx-0 pt-10 text-2xl text-center font-semibold '>Graphic Designs</div></Link>
                 </div>
                 <div className='lg:flex lg:flex-row lg:w-100% lg:pl-40' data-aos="fade-up">
-                <Link to="/design-lyrics"><div className='design-lyrics lg:w-96 text-2xl font-semibold border-2 border-red-100 mt-8 lg:mr-8 mx-4 lg:mx-0 '>Lyrics Videos</div></Link>
-                <Link to="/design-paper"><div className='design-art lg:w-96 text-2xl font-semibold border-2 border-red-100 mt-8 mx-4 lg:mx-0 '>Paper Art</div></Link>
+                <Link to="/design-lyrics"><div className='design-lyrics  border-2 lg:w-96 lg:mr-8 mt-8 lg:mx-0 pt-10 text-2xl text-center font-semibold '>Lyrics Videos</div></Link>
+                <Link to="/design-paper"><div className='design-art  border-2 lg:w-96 lg:mr-8 mt-8 lg:mx-0 pt-10 text-2xl text-center font-semibold '>Paper Art</div></Link>
                 </div>
                 
             </div>
--- src/component/WebsitePage.js
+++ src/component/WebsitePage.js
@@ -6,32 +6,60 @@ function WebsitePage() {
     return (
         <div className="">
             <WebHeader/>
-            <div className="flex">
+            <div className="flex py-2">
                 <h1 className="font-bold text-3xl lg:mx-14 my-6 px-4 text-center">W<br></br>E<br></br>B<br></br>S<br></br>I<br></br>T<br></br>E<br></br>S</h1>
                 <div className="lg:ml-14 text-center">
-                        <a href="https://stoic-nobel-14817f.netlify.app" ><div className="mt-8">
+                    <div className="flex rounded shadow mt-8 w-60 h-20">
+                        <p className="mx-6 text-4xl">1.</p>
+                        <div>
                             <h1>BoyBetterPray</h1>
-                      
+                            <a href="https://stoic-nobel-14817f.netlify.app" target="_" >
+                                <button className="rounded-xl border-2 border-green-600 mt-2 px-4 text-green-600 hover:text-gray-400">Veiw Site</button>
+                            </a>
+                        </div>
+                    </div>
 
-                        </div>  </a>
-                        <div className="mt-8">
-                            <p>portfolio</p>
+                    <div className="flex rounded shadow mt-8 w-60 h-20">
+                        <p className="mx-6 text-4xl">2.</p>
+                        <div>
+                            <h1>Portfolio</h1>
+                            <a href="/" target="_" >
+                                <button className="rounded-xl border-2 border-green-600 mt-2 px-4 text-green-600 hover:text-gray-400">Veiw Site</button>
+                            </a>
                         </div>
-                       
-                            <div className="mt-8">
-                                <p>Task Tracker App</p>
-                                <p>An app that allows you add, delete and set reminder for all your tasks</p>
-                                <a href="https://trasker.netlify.app" target="_"> <button className="border-2 px-2">Veiw App</button> </a>
-                            </div>
-                       
-                        <a href="https://nifty-panini-23115f.netlify.app/">
-                            <div className="mt-8">
-                                <p>One Big Family</p>
-                            </div>
-                        </a>
-                        <div className="mt-8">
-                            <p>MyKidismart App</p>
+                    </div>
+                    
+                    <div className="flex rounded shadow mt-8 w-60 h-20">
+                        <p className="mx-6 text-4xl">3.</p>
+                        <div>
+                            <h1>Task Tracker App</h1>
+                            <a href="https://trasker.netlify.app" target="_" >
+                                <button className="rounded-xl border-2 border-green-600 mt-2 px-4 text-green-600 hover:text-gray-400">Veiw App</button>
+                            </a>
                         </div>
+                    </div>
+                    
+
+                    <div className="flex rounded shadow mt-8 w-60 h-20">
+                        <p className="mx-6 text-4xl">4.</p>
+                        <div>
+                            <h1>One Big Family</h1>
+                            <a href="https://nifty-panini-23115f.netlify.app/" target="_" >
+                                <button className="rounded-xl border-2 border-green-600 mt-2 px-4 text-green-600 hover:text-gray-400">Veiw Site</button>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div className="flex rounded shadow mt-8 w-60 h-20">
+                        <p className="mx-6 text-4xl">5.</p>
+                        <div>
+                            <h1 className="text-lg font-semibold">MyKidismart App</h1>
+                            <a href="/" target="_" >
+                                <button className="rounded-xl border-2 border-green-600 mt-2 px-4 text-green-600 hover:text-gray-400">Veiw Site</button>
+                            </a>
+                        </div>
+                    </div>
+                       
                 </div>
             </div>
            
