9a9674f02c5bd243fc143b246f151cc0c4ec4e19
Annalartey <74235952+Annalartey@users.noreply.github.com>
2023-05-13 14:29:32 +0000

    Update Main.js

--- src/component/Main.js
+++ src/component/Main.js
@@ -16,7 +16,7 @@ function Main() {
                 <div data-aos="fade-right" className=" lg:px-36">
                     <div className='flex hidden justify-between text-lg text-red-300 pr-40 pb-40 lg:block'> <p> ~~ (+233) 0544929176  </p>     <p> ~~ anna.t.lartey@gmail.com</p></div>
                     <p className='text-2xl text-center font-sembold lg:text-left'>Hello, My name is</p>
-                    <h1 className="text-4xl text-center lg:text-left lg:text-6xl mb-2 pt-8 font-semibold">Anna Lartey</h1>
+                    <h1 className="text-4xl text-center lg:text-left lg:text-8xl mb-2 pt-8 font-semibold">Anna Lartey</h1>
                     <div className="w-40 lg:w-40 h-1 bg-red-300 my-2 lg:my-4 rounded-lg "></div>
                     <div className="w-40 lg:w-40 h-1 bg-red-300 ml-6 lg:ml-14 mb-10 rounded-2xl"></div>
                     <p className="text-center lg:text-left lg:text-xl">Frontend Software Developer / Graphic and Web Designer</p>
