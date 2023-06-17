780dbb6feea09c14ab735fff527baeec6a2113f7
AnnaLartey <anna.t.lartey@gmail.com>
2023-06-17 07:49:26 +0000

    added downloadable cv

--- src/component/Main.js
+++ src/component/Main.js
@@ -3,6 +3,7 @@ import Aos from 'aos';
 import "aos/dist/aos.css"
 import PortfolioText from './PortfolioText';
 import port1 from '../images/port1.png'
+import resume from '../images/resume.pdf'
 
 
 function Main() {
@@ -19,7 +20,9 @@ function Main() {
                     <div className="w-40 lg:w-40 h-1 bg-red-500 my-2 lg:my-4 rounded-lg "></div>
                     <div className="w-40 lg:w-40 h-1 bg-red-500 ml-6 lg:ml-14 mb-10 rounded-2xl"></div>
                     <p className="text-center lg:text-left lg:text-xl">Frontend Software Developer / Graphic and Web Designer</p>
-                    <button className='bg-white rounded border border-red-400 px-6 py-2 hover:bg-red-400'>download CV</button>
+                    <a href={resume} download='resume' >
+                        <button className='bg-white rounded border border-red-400 px-6 py-2 hover:bg-red-400'>download CV</button>
+                    </a>
                 </div>
                 <div data-aos="fade-down-left" data-aos-duration="3000" className="text-center font-bold text-4xl lg:text-6xl text-black-200 lg:mr-40 lg:py-0 lg:pt-20">
                     <img src={port1} alt="girl"/>
